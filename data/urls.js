d7a854b1a361031aea370f8df32c2c929b8acb25
talgoshen <talgoshen.service@gmail.com>
2026-02-24 12:19:42 +0200

    Apply mentor review fixes2

--- data/urls.js
+++ data/urls.js
@@ -1,4 +1,3 @@
-// I used baseURL in the playwright.config.js file, so I can use relative URLs in my tests. This makes the code cleaner and easier to maintain. If I need to change the base URL, I only need to do it in one place.
 export const URLS = {
   inventory: '/inventory.html',
   cart: '/cart.html',
--- data/users.js
+++ data/users.js
@@ -1,4 +1,3 @@
-// Don't use snake case for variables in JavaScript, use camelCase instead.
 export const Positive_users = [
   {username: 'problem_user', password: 'secret_sauce'},
   {username: 'performance_glitch_user', password: 'secret_sauce'},
